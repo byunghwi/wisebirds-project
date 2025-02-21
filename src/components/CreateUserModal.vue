b0565dd5d6335b01b6370c03f4ac1b89223a4037
병휘 <gpdldy91@gmail.com>
2025-02-21 19:47:52 +0900

    비밀번호 확인 마스킹 버튼 cursor 표시

--- src/components/CreateUserModal.vue
+++ src/components/CreateUserModal.vue
@@ -45,13 +45,13 @@
           <input :type="confirmType" v-model="confirmPassword" class="border p-2 w-full rounded pr-8 box-border"
             :class="{ 'border-red-500': confirmError }" placeholder="영문, 숫자, 특수문자 조합 8~15자" />
           <svg v-if="confirmType == 'password'" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-            fill="currentColor" class="bi bi-eye-fill absolute right-2 " viewBox="0 0 16 16"
+            fill="currentColor" class="bi bi-eye-fill absolute right-2 cursor-pointer" viewBox="0 0 16 16"
             @click="changeInputType('confirmType')">
             <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
             <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
           </svg>
           <svg v-else-if="confirmType == 'text'" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-            fill="currentColor" class="bi bi-eye-slash absolute right-2 " viewBox="0 0 16 16"
+            fill="currentColor" class="bi bi-eye-slash absolute right-2 cursor-pointer" viewBox="0 0 16 16"
             @click="changeInputType('confirmType')">
             <path
               d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
